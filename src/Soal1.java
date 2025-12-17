501add90736f1f5650f690ebce2582320de61b61
airintania19-commits <airintania19@gmail.com>
2025-12-17 16:58:46 +0700

    pair 02
    selesai

--- src/Soal1.java
+++ src/Soal1.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.lang.Math;
 
-public class Soal1{
+public class JavaApplication {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
